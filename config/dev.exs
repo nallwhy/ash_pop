e7b4038f09a89029bf8a1ee71bbfa1bc38794c30
Jinkyou Son <nallwhy@gmail.com>
2026-03-22 17:39:53 +0900

    Setup mise

--- config/dev.exs
+++ config/dev.exs
@@ -55,10 +55,10 @@ config :ash_pop, AshPopWeb.Endpoint,
     web_console_logger: true,
     patterns: [
       # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
       # Router, Controllers, LiveViews and LiveComponents
-      ~r"lib/ash_pop_web/router\.ex$"E,
-      ~r"lib/ash_pop_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r"lib/ash_pop_web/router\.ex$",
+      ~r"lib/ash_pop_web/(controllers|live|components)/.*\.(ex|heex)$"
     ]
   ]
 
